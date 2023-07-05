2b875afcc9b41cda55fd8c30dc034188b6d6602f
晓康 <xxk268858@antgroup.com>
2023-07-05 15:40:45 +0800

    remove hash in cloud

--- config/config.cloudweb.js
+++ config/config.cloudweb.js
@@ -12,6 +12,7 @@ export default {
     slave: {},
   },
   runtimePublicPath: {},
+  hash: false,
   // publicPath: 'https://gw.alipayobjects.com/as/g/oceanbase/sqlConsole/renderer/',
   // 路由配置，客户端不需要 Login
   routes: routes
