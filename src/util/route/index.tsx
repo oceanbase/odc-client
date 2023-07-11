e0aff046727a012ec65754ba36e77ff31f0ef1dc
晓康 <xxk268858@antgroup.com>
2023-07-11 14:19:04 +0800

    补充跳转的pathname

--- src/util/route/index.tsx
+++ src/util/route/index.tsx
@@ -1,6 +1,7 @@
 export function gotoSQLWorkspace(projectId?: number, datasourceId?: number, databaseId?: number) {
   window.open(
     location.origin +
+      location.pathname +
       `#/sqlworkspace?projectId=${projectId || ''}&datasourceId=${datasourceId || ''}&databaseId=${
         databaseId || ''
       }`,
