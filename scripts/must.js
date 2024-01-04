b93df94fd48caf020a39d5411d9c2fcef592f1c8
陈朝俊 <ccj01609335@oceanbase.com>
2024-01-04 19:31:14 +0800

    PullRequest: 297 deps: modify oic version and must.js
    Merge branch 'deps/dev-4.2.4-oicV2Config of git@code.alipay.com:oceanbase/oceanbase-developer-center.git into dev-4.2.4
    
    https://code.alipay.com/oceanbase/oceanbase-developer-center/pull_requests/297
    
    
    Signed-off-by: 晓康 <xxk268858@oceanbase.com>
    
    
    * deps: upgrade oic's version and modify must.js
    
    * fix: delete unused script
    
    * fix: revert package.json
    
    * fix: clear deprecated copywritings
    
    * fix: modify injectContent.headComment.hasHeadComment value
    
    * fix: pnpm install & revert @oceanbase-odc/monaco-plugin-ob version

--- scripts/must.js
+++ scripts/must.js
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-const must = require('@oceanbase-odc/ob-intl-cli');
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
@@ -25,7 +24,7 @@ const baseDir = path.join(__dirname, '..');
 
 const localePath = path.join(baseDir, 'src/locales');
 
-const outputPath = path.join(localePath, './must');
+const outputPath = path.join(localePath, './must/strings');
 
 const exclude = 'src/main';
 
@@ -40,17 +39,29 @@ function matchText(text, path) {
       path.parentPath.parentPath.parentPath.parentPath.parentPath.toString(),
     );
   } catch (e) {}
-  return /[^\x00-\xff]/.test(text) 
-  // && !isConsoleLog && !isFormattedMessage; // ^\x00-\xff 表示匹配中文字符
+  return /[\u{4E00}-\u{9FFF}]+(?![\u3000-\u303F\uFF01-\uFF5E])/gumi.test(text) && !isConsoleLog;
 }
-
+const commentContent = 
+`/*
+ * Copyright 2023 OceanBase
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */\n`;
 const config = {
-  cwd: baseDir,
   name: pkg.name,
   entry: 'src',
-  fileType: 'ts',
-  sourceLang: 'zh-CN',
-  prettier: true,
+  output: outputPath,
+  sep: '.',
   exclude: (path) => {
     return (
       path.includes('src/.umi') ||
@@ -58,133 +69,18 @@ const config = {
       (!!exclude && path.includes(exclude))
     ); // 不处理 .umi 下的文件
   },
-  matchCopy: matchText,
-  macro: {
-    path: outputPath,
-    method: `formatMessage({id: '$key$'})`,
+  sourceLang: 'zh-CN',
+  targetLang: 'en-US',
+  matchFunc: matchText,
+  injectContent: {
     import: "import { formatMessage } from '@/util/intl';\n",
-    showComment: true,
-  },
-  hook: {
-    beforeExtract: (sourceAST, absoluteFilePath, config) => {
-      if (absoluteFilePath.indexOf('/store/task') > -1) {
-        console.log(sourceAST); // 当前文件的 AST
-        console.log(absoluteFilePath); // 文件路径
-        console.log(config); // must 配置
-      }
-      return sourceAST; // 必须返回新的 AST
-    },
-    afterExtract: (injectedAST, keyMap, absoluteFilePath, config) => {
-      if (absoluteFilePath.indexOf('/store/test') > -1) {
-        console.log(injectedAST); // 当前文件的 AST
-        console.log(keyMap); // 提取的文案列表
-        console.log(absoluteFilePath); // 文件路径
-        console.log(config); // must 配置
-      }
-      return injectedAST; // 返回修改后的 AST
-    },
+    importPath: '@/util/intl',
+    method: `formatMessage({id: '$key$' })`,
+    headComment: {
+      hasHeadComment: false,
+      commentContent,
+    }
   },
 };
 
-const mode = process.argv[2];
-
-async function run() {
-  await must.run(config, true);
-  // languages.forEach((language) => {
-  //   const jsonPath = path.join(outputPath, 'strings', language + '.json');
-  //   if (fs.existsSync(jsonPath)) {
-  //     cpx(jsonPath, localePath);
-  //   }
-  // });
-  // fs.unlinkSync(path.join(outputPath, 'index.ts'));
-  // fs.unlinkSync(path.join(outputPath, 'strings/index.d.ts'));
-  // fs.unlinkSync(path.join(outputPath, 'strings/index.js'));
-}
-
-async function clear() {
-  await must.clear(config, true);
-  // languages.forEach((language) => {
-  //   const jsonPath = path.join(outputPath, 'strings', language + '.json');
-  //   if (fs.existsSync(jsonPath)) {
-  //     cpx(jsonPath, localePath);
-  //   }
-  // });
-  // fs.unlinkSync(path.join(outputPath, 'index.ts'));
-  // fs.unlinkSync(path.join(outputPath, 'strings/index.d.ts'));
-  // fs.unlinkSync(path.join(outputPath, 'strings/index.js'));
-}
-
-async function online() {
-  await must.import.run({
-    type: 'json',
-    path: outputPath,
-    medusa: {
-      appName: 'odc',
-    },
-  });
-  medusaCheck();
-}
-
-function medusaCheck() {
-  const baseline = path.join(outputPath, 'strings', 'zh-CN.json');
-  const validTargets = [
-    path.join(outputPath, 'strings', 'en-US.json'),
-    path.join(outputPath, 'strings', 'zh-TW.json'),
-  ];
-  try {
-    function getAllKeyRow(data) {
-      Object.keys(data).map((key) => {
-        const value = data[key];
-        if (!value.match) {
-          console.log('empty value ', value, '-', key);
-        }
-        const params = value.match(/\{[^}\s]+\}/g);
-        if (params && params.length) {
-          data[key] = {
-            params,
-            value,
-          };
-        } else {
-          delete data[key];
-        }
-      });
-      return data;
-    }
-    const baselineData = getAllKeyRow(JSON.parse(fs.readFileSync(baseline).toString()));
-    validTargets.forEach((targetPath) => {
-      const targetData = getAllKeyRow(JSON.parse(fs.readFileSync(targetPath).toString()));
-      const baselineKeys = Object.keys(baselineData);
-      const targetKeys = Object.keys(targetData);
-      if (baselineKeys.length !== targetKeys.length) {
-        console.error(
-          `[error] 词条总量不一致, ${targetPath}\n count: ${targetPath.length} expect: ${baselineKeys.length}`,
-        );
-      }
-      baselineKeys.forEach((baselineKey) => {
-        const { params, value } = baselineData[baselineKey] || {};
-        const { params: targetParams, value: targetValue } = targetData[baselineKey] || {};
-        if (!_.isEqual(params?.sort(), targetParams?.sort())) {
-          console.error(
-            `[error] ${targetPath} \n词条参数不一致 ${baselineKey} \n src: ${targetValue} \n zh-CN: ${value}`,
-          );
-        }
-      });
-    });
-  } catch (e) {
-    console.error('解析错误', e);
-    process.exit(1);
-  }
-}
-
-if (mode === 'online') {
-  online();
-} else if (mode === 'check') {
-  medusaCheck();
-} else if (mode === 'clear') {
-  clear()
-} else if (mode) {
-  console.error(`${mode} is invalid`)
-  process.exit(1);
-} else {
-  run();
-}
+module.exports = config;
