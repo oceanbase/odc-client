d7ee4d2cf88177003c9aeb61697467596268ede1
xiaokang <xxk268858@antgroup.com>
2024-03-11 09:32:59 +0800

    add sourcemap

--- config/config.js
+++ config/config.js
@@ -12,6 +12,8 @@ console.log('git last commit: ', version);
  */
 const disableBrowserUpdate = process.env.DISABLE_BROWSER_UPDATE;
 
+let enableSourceMap = false;
+
 console.log(disableBrowserUpdate);
 
 let publicPath = '/';
@@ -36,6 +38,7 @@ const config = {
       content: version,
     },
   ],
+  devtool: enableSourceMap ? "hidden-cheap-module-source-map" : (process.env.NODE_ENV === 'development' ? 'cheap-module-source-map' : false),
 
   antd: {
     import: true,
