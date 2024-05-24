e44525bb8fb4876e0f5eb241f2f9a7f076cdd7f8
p <ninalong.lnn@digital-engine.com>
2024-05-24 10:33:39 +0800

    origin view btn

--- src/component/Flow/customComponents/CustomControl.tsx
+++ src/component/Flow/customComponents/CustomControl.tsx
@@ -1,17 +1,18 @@
 import ReactFlow, { Controls, ControlButton, useReactFlow } from 'reactflow';
+import { OneToOneOutlined } from '@ant-design/icons';
+import { transformDataForReactFlow, initCenter, handleSelectNode } from '../utils';
 
 export default function Flow() {
-  const { zoomIn, zoomOut, fitView } = useReactFlow();
+  const { zoomIn, zoomOut, fitView, setCenter } = useReactFlow();
   return (
     <Controls>
-      {/* todo  实际尺寸 与 适应宽度 */}
-      {/* <ControlButton
+      <ControlButton
         onClick={() => {
-          zoomOut();
+          initCenter(setCenter);
         }}
       >
-        ++
-      </ControlButton> */}
+        <OneToOneOutlined />
+      </ControlButton>
     </Controls>
   );
 }
