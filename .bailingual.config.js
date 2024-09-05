809fa676ae2b8d2a9db99eb03062fc8c296c8e7a
xiaokang <xxk268858@antgroup.com>
2024-09-05 14:51:14 +0800

    chore: blg import

---
+++ .bailingual.config.js
@@ -0,0 +1,12 @@
+// more config: https://yuque.antfin.com/bailingual/guides/cli-config
+/** @type {import('@alipay/bailingual-cli').BailingualConfig} */
+module.exports = {
+  import: {
+    type: 'json',
+    path: 'src/locales/must/strings',
+    bailingual: {
+      appName: 'oceanbase-developer-center',
+      tag: '',
+    },
+  },
+};
