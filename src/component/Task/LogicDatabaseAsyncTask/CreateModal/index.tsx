a304c5c7cb4cc31da1246b8b7614e02237d24f2a
畅晚 <ninalong.lnn@digital-engine.com>
2025-04-30 18:11:43 +0800

    PullRequest: 787 fix: add alert message in logical change task
    Merge branch 'fix/dev-4.3.4 of git@code.alipay.com:oceanbase/oceanbase-developer-center.git into dev-4.3.4
    
    https://code.alipay.com/oceanbase/oceanbase-developer-center/pull_requests/787
    
    
    Signed-off-by: 艺泽 <yize.ly@digital-engine.com>
    
    
    * fix: add alert message in logical change task

--- src/component/Task/LogicDatabaseAsyncTask/CreateModal/index.tsx
+++ src/component/Task/LogicDatabaseAsyncTask/CreateModal/index.tsx
@@ -275,10 +275,16 @@ const CreateModal: React.FC<IProps> = (props) => {
         <Alert
           type="info"
           showIcon
-          message={formatMessage({
-            id: 'src.component.Task.LogicDatabaseAsyncTask.CreateModal.435C79D4',
-            defaultMessage: '逻辑库变更仅支持 DDL 语句，多条 SQL 将依次在所有实际数据库上执行。',
-          })}
+          message={
+            <Space direction="vertical">
+              {formatMessage({
+                id: 'src.component.Task.LogicDatabaseAsyncTask.CreateModal.435C79D4',
+                defaultMessage:
+                  '逻辑库变更仅支持 DDL 语句，多条 SQL 将依次在所有实际数据库上执行。',
+              })}
+              逻辑库变更内容暂不支持注释解析，编写注释可能会导致 SQL 语句解析异常。
+            </Space>
+          }
         />
 
         <Form
